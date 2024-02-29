baae2faf4274d34c37acb5f577ae4793ee2929e9
Nayeem-condify <webdevlopment2306@gmail.com>
2024-02-29 13:47:42 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,27 @@
+for (let i = 1; i <= 10; i++) {
+    console.log("Sorry" + i);
+}
+
+let students = ["html", "Css", "javascript", "react", "react router dom", "dom", "Bom"]
+
+for (let i = 0; i < students.length; i++) {
+    console.log(`${students[i]} ${i}`);
+}
+
+let namata = 10
+
+for(let nayeem = 1; nayeem <= 10; nayeem++){
+     console.log(namata + " X " + nayeem + " = " + namata*nayeem);
+}
+
+
+let webdevlopment = ["Html", "Css", "Tailwind", "Javascript", "React"] 
+
+let newarry = webdevlopment.pop
+console.log(webdevlopment);
+
+
+
+let webdevlopments = ["Html", "Css", "Tailwind", "Javascript", "React"] 
+let  newarr = webdevlopments.splice(2,1, "Bom" , "Dom")
+console.log(webdevlopments);
